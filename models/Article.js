5f3326bf395c866307c3b978a5415043fbdb78fa
Hari <augten95@gmail.com>
2019-10-17 13:47:12 -0500

    Created a mongoose schema for articles and exporting the Article mongoose model

---
+++ models/Article.js
@@ -0,0 +1,41 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var Articlesschema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String, 
+        required: true,
+    },
+    summary: {
+        type: String,
+        default: "Summary unavailable."
+    },
+    img: {
+        type: String,
+    },
+    issaved: {
+        type: Boolean,
+        default: false
+    },
+    status: {
+        type: String, 
+        default: "Save Article"
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+Articlesschema.index({title: "text"});
+
+var Article = mongoose.model("Article", Articlesschema);
+mondule.exports = Article;
