cdc138cbe5fc27527f6ee0ccfe0ae178b40a326b
Hari <augten95@gmail.com>
2019-10-18 14:55:16 -0500

    added app.set('view engine', 'handlebars');

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
